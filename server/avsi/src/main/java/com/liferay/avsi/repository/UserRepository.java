135363a54ef00acfc830097e7746fd15591fa778
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-08 14:28:03 -0300

    add method to authenticate user

--- server/avsi/src/main/java/com/liferay/avsi/repository/UserRepository.java
+++ server/avsi/src/main/java/com/liferay/avsi/repository/UserRepository.java
@@ -4,13 +4,15 @@ import com.liferay.avsi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author Leonardo Barros
  */
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
-	List<String> findByName(@Param("name") String name);
+
+	@RestResource(exported = false)
+	User findByEmailAndAndPassword(
+		@Param("email") String email, @Param("password") String password);
 }
