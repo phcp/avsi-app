39c125f9928867911a93d971e7a84ac5e487ac7d
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-08 14:29:27 -0300

    add authentication

---
+++ server/avsi/src/main/java/com/liferay/avsi/security/CustomAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.liferay.avsi.security;
+
+import com.liferay.avsi.entity.User;
+import com.liferay.avsi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+
+		String name = authentication.getName();
+		String password = authentication.getCredentials().toString();
+
+		password = passwordEncoder.encode(password);
+
+		User user = userRepository.findByEmailAndAndPassword(name, password);
+
+		if (user != null) {
+			return new UsernamePasswordAuthenticationToken(
+				name, password, Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+}
