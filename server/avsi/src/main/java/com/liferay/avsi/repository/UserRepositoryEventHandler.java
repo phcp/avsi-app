fcc1d70b76f598e46e92d52ca4b44aaf701106d1
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-08 14:28:47 -0300

    encode password when inserting a user

--- server/avsi/src/main/java/com/liferay/avsi/repository/UserRepositoryEventHandler.java
+++ server/avsi/src/main/java/com/liferay/avsi/repository/UserRepositoryEventHandler.java
@@ -1,25 +1,30 @@
 package com.liferay.avsi.repository;
 
 import com.liferay.avsi.entity.User;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Leonardo Barros
  */
+@Component
 @RepositoryEventHandler(User.class)
 public class UserRepositoryEventHandler {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@HandleBeforeSave
 	public void handleUserSave(User user) {
-		user.setPassword(passwordEncoder().encode(user.getPassword()));
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	@HandleBeforeCreate
+	public void handleUserCreate(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 	}
 }
