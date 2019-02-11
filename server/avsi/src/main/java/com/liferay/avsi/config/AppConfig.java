0aeb83497762629b4aa6aef6941e8fd37f98a5f2
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-11 15:13:18 -0300

    add message digest encoder

--- server/avsi/src/main/java/com/liferay/avsi/config/AppConfig.java
+++ server/avsi/src/main/java/com/liferay/avsi/config/AppConfig.java
@@ -2,8 +2,11 @@ package com.liferay.avsi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Leonardo Barros
@@ -13,6 +16,39 @@ public class AppConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence charSequence) {
+				return getMd5(charSequence.toString());
+			}
+
+			@Override
+			public boolean matches(CharSequence charSequence, String s) {
+				return getMd5(charSequence.toString()).equals(s);
+			}
+		};
+	}
+
+	static String getMd5(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			String hashtext = no.toString(16);
+
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+
+			return hashtext;
+		}
+		catch (NoSuchAlgorithmException e) {
+			System.out.println("Exception thrown"
+					+ " for incorrect algorithm: " + e);
+			return null;
+		}
 	}
 }
