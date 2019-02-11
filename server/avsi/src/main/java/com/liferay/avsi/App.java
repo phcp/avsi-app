7fac413fe3ce1dda1e2b83591c56709e30e842b6
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-11 15:12:39 -0300

    enable auditing

--- server/avsi/src/main/java/com/liferay/avsi/App.java
+++ server/avsi/src/main/java/com/liferay/avsi/App.java
@@ -2,11 +2,13 @@ package com.liferay.avsi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author Leonardo Barros
  */
 @SpringBootApplication
+@EnableJpaAuditing
 public class App {
     public static void main(String[] args) {
     	SpringApplication.run(App.class, args);
