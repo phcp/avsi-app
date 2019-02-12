6510b3c52872263d9cfa1ef3812a16007cc653b6
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-12 09:18:06 -0300

    not needed

--- server/avsi/src/main/java/com/liferay/avsi/repository/UserRepository.java
+++ server/avsi/src/main/java/com/liferay/avsi/repository/UserRepository.java
@@ -3,14 +3,11 @@ package com.liferay.avsi.repository;
 import com.liferay.avsi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author Leonardo Barros
  */
 public interface UserRepository extends JpaRepository<User, Long> {
-
-	@RestResource(exported = false)
 	User findByEmailAndAndPassword(
 		@Param("email") String email, @Param("password") String password);
 }
